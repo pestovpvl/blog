5784852dc95c8cab931395414a7b65cd64d71ca4
pestovpvl <pestovpvl@mail.ru>
2018-04-05 11:31:27 -0400

    Implemnt articles show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,9 @@ class ArticlesController < ApplicationController
   def new
 
   end
+  def show
+    @article = Article.find(params[:id])
+  end
 
   def create
     @article = Article.new(article_params)
