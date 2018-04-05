c7298062da1aeba2dba005e9e3a9c571a861dde4
pestovpvl <pestovpvl@mail.ru>
2018-04-04 23:03:52 -0400

    Change /contacts/new to /contacts

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
 
   resources :articles
 
-  resources :contacts, only: [:new, :create]
+  resources :contacts, only: [:new, :create], path_names: { :new => '' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
