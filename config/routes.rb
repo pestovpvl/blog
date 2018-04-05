0b18a23b675b014571fb7dcea78010fb2d76250a
pestovpvl <pestovpvl@mail.ru>
2018-04-04 21:13:50 -0400

    Add contacts controller and html view

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
 
   resources :articles
+
+  resources :contacts, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
