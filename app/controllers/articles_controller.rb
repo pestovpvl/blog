c1474473c2239bbb95a1d6032cdc7e7656cd27b1
pestovpvl <pestovpvl@mail.ru>
2018-04-06 00:03:14 -0400

    Implement articles delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,6 +20,12 @@ class ArticlesController < ApplicationController
     end
   end
 
+   def destroy 
+      @article = Article.find(params[:id])
+      @article.destroy
+
+      redirect_to articles_path
+   end
 
   def edit
     @article = Article.find(params[:id])
