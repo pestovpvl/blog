7f19df7fe36f007a6190d3e324ee29d36513c50a
pestovpvl <pestovpvl@mail.ru>
2018-04-10 20:59:07 -0400

    Fixed begfore action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
 
-     before_action :authenticate_user!, only: [:new, :create]
+     before_action :authenticate_user!, :only => [:new, :create]
     def create
         @article = Article.find(params[:article_id])
         @article.comments.create(comment_params)
